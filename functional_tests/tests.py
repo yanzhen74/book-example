b27744490ebaf6b7216c19d11cab358ff75e9cb1
yanzhen74 <yanzhen@csu.ac.cn>
2017-12-07 22:21:22 +0800

    add supervisor conf

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -9,16 +9,19 @@ import unittest
 class NewVisitorTest(StaticLiveServerTestCase):
     @classmethod
     def setUpClass(cls):
+        cls.use_live_server = True
         for arg in sys.argv:
             if 'liveserver' in arg:
                 cls.server_url = 'http://' + arg.split('=')[1]
+                cls.use_live_server = False
                 return
         super().setUpClass()
         cls.server_url = cls.live_server_url
 
     @classmethod
     def tearDownClass(cls):
-        if cls.server_url == cls.live_server_url:
+        # if cls.server_url == cls.live_server_url:
+        if cls.use_live_server:
             super().tearDownClass()
 
     def setUp(self):
