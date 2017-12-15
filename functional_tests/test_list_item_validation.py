f8371f2c96fb76fed21aed96e78f04906eca4473
yanzhen74 <yanzhen@csu.ac.cn>
2017-12-15 21:43:25 +0800

    add js hide erro-msg test

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -3,6 +3,9 @@ from .base import FunctionalTest
 
 
 class ItemValidationTest(FunctionalTest):
+    def get_error_element(self):
+        return self.browser.find_element_by_css_selector('.has-error')
+
     def test_cannot_add_empty_list_items(self):
         print(self.server_url)
         self.browser.get(self.server_url+'/lists/new')
@@ -11,7 +14,7 @@ class ItemValidationTest(FunctionalTest):
         time.sleep(1)
 
         print(self.browser.page_source)
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You can't have an empty list item")
 
         self.get_item_input_box().send_keys('Buy milk\n')
@@ -35,5 +38,16 @@ class ItemValidationTest(FunctionalTest):
         self.get_item_input_box().send_keys('Buy wellies\n')
 
         self.check_for_row_in_list_table('1: Buy wellies')
-        error = self.browser.find_element_by_css_selector('.has-error')
+        error = self.get_error_element()
         self.assertEqual(error.text, "You've already got this in your list")
+
+    def test_error_messages_are_cleared_on_input(self):
+        self.browser.get(self.server_url+'/lists/new')
+        self.get_item_input_box().send_keys('\n')
+        error = self.get_error_element()
+        self.assertTrue(error.is_displayed())
+
+        self.get_item_input_box().send_keys('a')
+
+        error = self.get_error_element()
+        self.assertFalse(error.is_displayed())
