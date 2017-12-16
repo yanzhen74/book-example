148ade9577b9d31ebe001538efe8324d31dde3fd
yanzhen74 <yanzhen@csu.ac.cn>
2017-12-16 22:02:36 +0800

    use form input to limit input

--- lists/static/list.js
+++ lists/static/list.js
@@ -1,5 +1,7 @@
-$('input').on('keypress', function(e){
-    if (e.keyCode != 13) {
-        $('.has-error').hide();
-    }
+jQuery(document).ready(function($) {
+    $('form input').on('keypress', function (e) {
+        if (e.keyCode != 13) {
+            $('.has-error').hide();
+        }
+    });
 });
