cced31d2f4d9b85cb8a77e20dda1ce02478578fe
yanzhen74 <yanzhen@csu.ac.cn>
2017-12-17 21:37:26 +0800

    test account

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,13 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.conf.urls import url
 from django.contrib import admin
-import lists.views
+import accounts.views
 
 urlpatterns = [
-    # url(r'^admin/', admin.site.urls),
-    url(r'^$', lists.views.home_page, name='home'),
-    url(r'^lists/', include('lists.urls')),
-    url(r'^accounts/', include('accounts.urls')),
+    url(r'^login$', accounts.views.login, name='login'),
+    url(r'^logout$', accounts.views.logout, name='logout'),
 ]
