ca90bcd42e8370684b556ab7e4b332e6daf3baaa
yanzhen74 <yanzhen@csu.ac.cn>
2017-12-18 22:46:04 +0800

    back to master

--- accounts/views.py
+++ accounts/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import redirect
 
 
 def login(request):
-    print('login view', file=sys.stderr)
+    # print('login view', file=sys.stderr)
     # user = PersonaAuthenticationBackend().authenticate(request.POST['assertion'])
     user = authenticate(assertion=request.POST['assertion'])
     if user is not None:
