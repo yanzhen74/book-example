463b9e543d9e391fba1eac7d27d8b5b4e2329bef
yanzhen74 <yanzhen@csu.ac.cn>
2018-01-10 21:25:46 +0800

    New FT for sharing, move session creation stuff to base

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -10,6 +10,9 @@ import os
 from datetime import datetime
 from .server_tools import reset_database
 from lists.models import List, Item
+from django.conf import settings
+from .server_tools import create_session_on_server
+from .management.commands.create_session import create_pre_authenticated_session
 
 SCREEN_DUMP_LOCATION = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'screendumps'
@@ -32,6 +35,21 @@ def wait(fn):
 
 
 class FunctionalTest(StaticLiveServerTestCase):
+
+    def create_pre_authenticated_session(self, email):
+        if self.staging_server:
+            session_key = create_session_on_server(self.staging_server, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
+        ## to set a cookie we need to first visit the domain.
+        ## 404 pages load the quickest!
+        self.browser.get(self.live_server_url + "/404_no_such_rul/")
+        self.browser.add_cookie(dict(
+            name=settings.SESSION_COOKIE_NAME,
+            value=session_key,
+            path='/',
+        ))
+
     @classmethod
     def setUpClass(cls):
         cls.use_live_server = True
--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -9,19 +9,6 @@ User = get_user_model()
 
 
 class MyListsTest(FunctionalTest):
-    def create_pre_authenticated_session(self, email):
-        if self.staging_server:
-            session_key = create_session_on_server(self.staging_server, email)
-        else:
-            session_key = create_pre_authenticated_session(email)
-        ## to set a cookie we need to first visit the domain.
-        ## 404 pages load the quickest!
-        self.browser.get(self.live_server_url + "/404_no_such_rul/")
-        self.browser.add_cookie(dict(
-            name=settings.SESSION_COOKIE_NAME,
-            value=session_key,
-            path='/',
-        ))
 
     def test_logged_in_users_lists_are_saved_as_my_lists(self):
         email = 'yanzhen@csu.ac.cn'
